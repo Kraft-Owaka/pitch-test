9b0f10f1147181718c2ccd722270094a09fe1ba2
kraft-owaka <owakakraft@gmail.com>
2020-02-11 21:39:59 +0300

    adding a user profile

--- app/models.py
+++ app/models.py
@@ -5,3 +5,15 @@ class Pickup:
         self.pickup_id = pickup_id
         self.author = author
         self.pickupLine = pickupLine
+
+
+    __tablename__ = 'users'
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255),index = True)
+    email = db.Column(db.String(255),unique = True,index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
