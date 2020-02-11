11aae0c5dced69673403e263d6399a983a6adf3f
kraft-owaka <owakakraft@gmail.com>
2020-02-11 17:31:35 +0300

    first commit

---
+++ app/main/views.py
@@ -0,0 +1,16 @@
+from flask import render_template
+from . import main
+
+
+# Views
+@main.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    message = 'hello Wold'
+    return render_template('index.html',message = message) 
+
+    
