d224e402f378cd2807191ff0ca8ed9524d3c64b1
kraft-owaka <owakakraft@gmail.com>
2020-02-13 09:12:02 +0300

    added a Pitch form and defined the radio field

--- app/main/form.py
+++ app/main/form.py
@@ -1,13 +1,35 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,TextAreaField,SubmitField
+from wtforms import StringField,TextAreaField,SubmitField,RadioField
 from wtforms.validators import Required
 
-class PickupForm(FlaskForm):
+# class PickupForm(FlaskForm):
     
-    author = StringField('Pickup', validators = [Required()])
-    pickup = TextAreaField('Pickup line', validators = [Required()])
+#     author = StringField('Pickup', validators = [Required()])
+#     pickup = TextAreaField('Pickup line', validators = [Required()])
     
-    submit = SubmitField('Submit')
+#     submit = SubmitField('Submit')
+
+# class UpdateProfile(FlaskForm):
+#     bio = TextAreaField('Tell us about you.',validators = [Required()])
+#     submit = SubmitField('Submit')
+
+
+class PitchForm(FlaskForm):
+
+    title=StringField('Pitch title', validators=[Required()])
+    category=RadioField('Label',choices=[('investorpitch','investorpitch'),('productpitch','productpitch'),('interviewpitch','interviewpitch'),('marketpitch','marketpitch')],validators=[Required()])
+    description=TextAreaField('Enter your pitch', validators=[Required()])
+    submit=SubmitField('Submit')
+
+class CommentForm(FlaskForm):
+    description=TextAreaField('Leave a comment')
+    submit=SubmitField()
+
+class UpvoteForm(FlaskForm):
+    submit=SubmitField()
+
+class DownvoteForm(FlaskForm):
+    submit=SubmitField()
 
 class UpdateProfile(FlaskForm):
     bio = TextAreaField('Tell us about you.',validators = [Required()])
