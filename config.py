804edd22b6f5ac26daf7b919609f3741b3548071
kraft-owaka <owakakraft@gmail.com>
2020-02-15 07:20:38 +0300

    set the app to production

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://kraft:admin123@localhost/pitches"
+     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SECRET_KEY=os.environ.get('SECRET_KEY')
     SECRET_KEY='bc29199ac8b2b522fd951d97e0da5c62'
 
